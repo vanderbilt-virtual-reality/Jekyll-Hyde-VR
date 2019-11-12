d71e44fb7b43dd56c721adfd29c27baac47aa057
Conner Pinson <pinsonconner@gmail.com>
2019-11-12 16:02:59 -0600

    rest of the repo

---
+++ Assets/LevelSwitch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LevelSwitch : MonoBehaviour
+{
+    public int curLevel = 0;
+
+    public string[] levelNames = new string[3] { "House interior", "courtyard_scene", "Surgical Theater" };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.GetComponent<OVRGrabber>().grabbedObject != null)
+        {
+            print(gameObject.GetComponent<OVRGrabber>().grabbedObject.gameObject);
+            if (gameObject.GetComponent<OVRGrabber>().grabbedObject.gameObject.tag == "DoorEnter")
+            {
+                Debug.Log(gameObject.GetComponent<OVRGrabber>().grabbedObject.gameObject.tag);
+                Debug.Log(curLevel);
+                curLevel--;
+                Debug.Log(curLevel);
+                SteamVR_LoadLevel.Begin(levelNames[curLevel]);
+            }
+            else if (gameObject.GetComponent<OVRGrabber>().grabbedObject.gameObject.tag == "DoorExit")
+            {
+                Debug.Log(gameObject.GetComponent<OVRGrabber>().grabbedObject.gameObject.tag);
+                Debug.Log(curLevel);
+                curLevel++;
+                Debug.Log(curLevel);
+                SteamVR_LoadLevel.Begin(levelNames[curLevel]);
+            }
+        }
+        
+    }
+}
